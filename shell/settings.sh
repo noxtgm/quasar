5e5363e6fdf703ba5999f11896377ad43cfb8a17
noxtgm <contact@noxtgm.me>
2026-05-12 21:48:49 -0700

    v0.1.9

--- shell/settings.sh
+++ shell/settings.sh
@@ -3,6 +3,7 @@
 if [[ -z "$REPO_PATH" ]]; then
     REPO_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 fi
+
 export REPO_PATH
 
 [[ ":$PATH:" != *":${REPO_PATH}/bin:"* ]] && export PATH="${REPO_PATH}/bin:${PATH}"
